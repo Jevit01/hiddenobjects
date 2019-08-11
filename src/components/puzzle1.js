e023230a5a29af29887893310b05642e3ef81612
Thomas Perez <thomasperez@pursuit.org>
2019-08-11 00:09:00 -0400

    testing

--- src/components/puzzle1.js
+++ src/components/puzzle1.js
@@ -13,7 +13,8 @@ class Puzzle1 extends Component {
     kitten: false,
     goldKey: false,
     horseShoe: false,
-    dove: false
+    dove: false,
+    done: 0
   };
 
   handleHammerClick = () => {
@@ -76,30 +77,86 @@ class Puzzle1 extends Component {
     });
   };
 
+  gameDone = () => {
+    const {
+      hammer,
+      handCuff,
+      shoe,
+      spoon,
+      bullet,
+      scissors,
+      kitten,
+      goldKey,
+      horseShoe,
+      dove
+    } = this.state;
+    if (
+      hammer &&
+      handCuff &&
+      shoe &&
+      spoon &&
+      bullet &&
+      scissors &&
+      kitten &&
+      goldKey &&
+      horseShoe &&
+      dove === true
+    ) {
+      this.setState({
+        done: true
+      });
+    }
+    console.log(this.state.done);
+  };
+
   render() {
     return (
       <>
-        <img src={puzzle1} alt="" />
-        <button onClick={this.handleHammerClick}>hammer</button>
-        <button onClick={this.handleHandCuffClick}>HandCuff</button>
-        <button onClick={this.handleShoeClick}>Shoe</button>
-        <button onClick={this.handleSpoonClick}>Spoon</button>
-        <button onClick={this.handleBulletClick}>Bullet</button>
-        <button onClick={this.handleScissorsClick}>Scissors</button>
-        <button onClick={this.handleKittenClick}>Kitten</button>
-        <button onClick={this.handleGoldKeyClick}>GoldKey</button>
-        <button onClick={this.handleHorseShoeClick}>HorseShoe</button>
-        <button onClick={this.handleDoveClick}>Dove</button>
-        {this.state.hammer ? "" : <p>Hammer</p>}
-        {this.state.handCuff ? "" : <p>Hand Cuff</p>}
-        {this.state.shoe ? "" : <p>Shoe</p>}
-        {this.state.spoon ? "" : <p>Spoon</p>}
-        {this.state.bullet ? "" : <p>Bullet</p>}
-        {this.state.scissors ? "" : <p>Scissors</p>}
-        {this.state.kitten ? "" : <p>Kitten</p>}
-        {this.state.goldKey ? "" : <p>Gold Key</p>}
-        {this.state.horseShoe ? "" : <p>Horse Shoe</p>}
-        {this.state.dove ? "" : <p>Dove</p>}
+        <div className="puz1">
+          <img src={puzzle1} alt="" />
+        </div>
+        <button className="hammer" onClick={this.handleHammerClick}>
+          hammer
+        </button>
+        <button className="cuff" onClick={this.handleHandCuffClick}>
+          HandCuff
+        </button>
+        <button className="shoe" onClick={this.handleShoeClick}>
+          Shoe
+        </button>
+        <button className="spoon" onClick={this.handleSpoonClick}>
+          Spoon
+        </button>
+        <button className="bullet" onClick={this.handleBulletClick}>
+          Bullet
+        </button>
+        <button className="scissors" onClick={this.handleScissorsClick}>
+          Scissors
+        </button>
+        <button className="kitten" onClick={this.handleKittenClick}>
+          Kitten
+        </button>
+        <button className="key" onClick={this.handleGoldKeyClick}>
+          GoldKey
+        </button>
+        <button className="hshoe" onClick={this.handleHorseShoeClick}>
+          HorseShoe
+        </button>
+        <button className="dove" onClick={this.handleDoveClick}>
+          Dove
+        </button>
+        <div className="words">
+          {this.state.hammer ? "" : <p>Hammer</p>}
+          {this.state.handCuff ? "" : <p>Hand Cuff</p>}
+          {this.state.shoe ? "" : <p>Shoe</p>}
+          {this.state.spoon ? "" : <p>Spoon</p>}
+          {this.state.bullet ? "" : <p>Bullet</p>}
+          {this.state.scissors ? "" : <p>Scissors</p>}
+          {this.state.kitten ? "" : <p>Kitten</p>}
+          {this.state.goldKey ? "" : <p>Gold Key</p>}
+          {this.state.horseShoe ? "" : <p>Horse Shoe</p>}
+          {this.state.dove ? "" : <p>Dove</p>}
+        </div>
       </>
     );
   }
