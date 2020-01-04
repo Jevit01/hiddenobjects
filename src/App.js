fced56e85ab4c02dd75fe6e03847e758a38e27e4
Thomas Perez <thomasperez@pursuit.org>
2020-01-03 21:54:57 -0500

    homepage

--- src/App.js
+++ src/App.js
@@ -18,18 +18,23 @@ class App extends Component {
         {this.state.start ? (
           <Puzzle1 />
         ) : (
-          <>
-            <h1>Hidden Objects</h1>
-            <p>
-              Welcome to Hidden Objects. The objective of the game is to locate
-              all objects given to you in the word bank and simply click on the
-              image of that word. Be aware there is a time limit! If time runs
-              out you will have to restart back at this page and start all the
-              way to level 1. As levels go up, your time limit will go down.
-            </p>
-            <p>Happy Hunting!</p>
-            <button onClick={this.handleStart}>START</button>
-          </>
+          <div className="homepage">
+            <div className="box">
+              <h1 className="title">Hidden Objects</h1>
+              <div className="objective">
+                <p>
+                  Welcome to Hidden Objects. The objective of the game is to
+                  locate all objects given to you in the word bank and simply
+                  click on the image of that word. Be aware there is a time
+                  limit! If time runs out you will have to restart back at this
+                  page and start all the way from level 1. As levels go up, your
+                  time limit will go down.
+                </p>
+                <p>Happy Hunting!</p>
+              </div>
+              <button onClick={this.handleStart}>START</button>
+            </div>
+          </div>
         )}
       </div>
     );
